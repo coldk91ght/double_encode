22f915f4c4626444d51ba05e9adc0133553512b3
coldk91ght <37668193+coldk91ght@users.noreply.github.com>
2018-07-15 21:18:59 +0530

    Create double_encoder.py

---
+++ double_encoder.py
@@ -0,0 +1,23 @@
+###THE SYNTAX MUST BE --> double_encode(b'<your BASE64 string>') <-- ###
+###for example, IF YOU WANT TO DOUBLE ENCODE '<script>':
+#>>>     YOUR SYNTAX SHOULD BE: double_encode(b'<script>')     <<<#
+
+
+import binascii
+def double_encode(stri):
+	hexin = binascii.hexlify(stri)
+  hexin = str(hexin)[2:].upper()
+	i=0
+	ls=[]
+	while(i < len(hexin)):
+		if(i % 2==0):
+			try:
+				temp += str(hexin[i])
+			except:
+				temp = str(hexin[i])
+		else:
+			temp += str(hexin[i])
+			ls.append(r'%25'+str(temp))
+			temp=""
+		i+=1
+	return "".join(ls)
